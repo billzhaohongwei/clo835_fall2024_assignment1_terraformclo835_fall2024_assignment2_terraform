3af819a2fc097b932c773d0e95b101663d8a4506
EC2 Default User <ec2-user@ip-172-31-1-81.ec2.internal>
2024-10-25 19:14:41 +0000

    modified script to install docker and kind

--- install_kind.sh
+++ install_kind.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
 set -ex
+# log for troubleshooting
+exec > /home/ec2-user/user-data.log 2>&1
+
+# Install Docker
 sudo yum update -y
 sudo yum install docker -y
 sudo systemctl start docker
 sudo usermod -a -G docker ec2-user
+
+# Install kind
 curl -sLo kind https://kind.sigs.k8s.io/dl/v0.11.0/kind-linux-amd64
 sudo install -o root -g root -m 0755 kind /usr/local/bin/kind
 rm -f ./kind
+
+# Install Kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm -f ./kubectl
-kind create cluster --config kind.yaml
+
+# Create configuration file for kind cluster
+cat <<EOF > /home/ec2-user/kind.yaml
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  image: kindest/node:v1.19.11@sha256:07db187ae84b4b7de440a73886f008cf903fcf5764ba8106a9fd5243d6f32729
+  extraPortMappings:
+  - containerPort: 30000
+    hostPort: 30000
+  - containerPort: 30001
+    hostPort: 30001
+EOF
+
+sudo chown ec2-user:ec2-user /home/ec2-user/kind.yaml
+sleep 5
+
+# Create cluster
+sudo -u ec2-user kind create cluster --config /home/ec2-user/kind.yaml
+echo "Script completed."
